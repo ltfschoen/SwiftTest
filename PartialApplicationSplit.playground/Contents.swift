8f84377dc03acb6ea53c459b0a67ae81462e8221
Luke Schoen <ltfschoen@gmail.com>
2015-08-29 05:59:10 -0400

    Currying applied to Partial Application by shifting parameter usage to Outer function

--- PartialApplicationSplit.playground/Contents.swift
+++ PartialApplicationSplit.playground/Contents.swift
@@ -19,19 +19,27 @@ print(data.componentsSeparatedByString(";"))
 
 /* Functional Approach - Partial Application (generic solution) to processing delimited data, which is the process of "fixing" one or more arguments (i.e. fixing the separator parameter of componentsSeparatedByString) of a function to create a new function, using a Factory-like function that leverages First-Class Functions and Closures (capturing the separator argument within the Closure) where Signature takes a String and returns array of Strings by creating inner-function with the required return type that is implemented using componentsSeparatedByString to return a split string based on given separator
 */
-func createSplitter(separator:String) -> (String -> [String]) {
-    func split(source:String) -> [String] {
-        return source.componentsSeparatedByString(separator)
-    }
-    return split
+
+// Use Currying by changing to a Signature with two sets of parameters in parentheses that returns a new function with a separator parameter bound to the given value when invoked (instead of having to manually create the inner function that is returned from the outer 'createSplitter' function)
+func createSplitter(separator:String)(source:String) -> [String] {
+    return source.componentsSeparatedByString(separator)
 }
+
+//func createSplitter(separator:String) -> (String -> [String]) {
+//    func split(source:String) -> [String] {
+//        return source.componentsSeparatedByString(separator)
+//    }
+//    return split
+//}
 
 // Implementation of reusable Partial Application splitter function
 
-// Split type String by delimiter comma
+// Split type String by delimiter comma. Note 'source:' prefix name is required since with use of Currying the 'createSplitter' Function requires External Parameter Names
 let commaSplitter = createSplitter(",")
-print(commaSplitter(data))
+print(commaSplitter(source: data))
+//print(commaSplitter(data))
 
 // Split type String by delimiter semicolon
 let semiColonSplitter = createSplitter(";")
-print(semiColonSplitter(data))
+print(commaSplitter(source: data))
+//print(semiColonSplitter(data))
