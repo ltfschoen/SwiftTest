922d12794d712bdbdeac7d96230b8d6538398ecd
Luke Schoen <ltfschoen@gmail.com>
2015-09-02 10:31:00 -0400

    View Controller default for Single Page App

---
+++ FreeWifiSearch/FreeWifiSearch/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FreeWifiSearch
+//
+//  Created by LS on 2/09/2015.
+//  Copyright © 2015 LS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
