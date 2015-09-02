38b7e0c500abb00a8cb3cc1b34b3cb8800733cbd
Luke Schoen <ltfschoen@gmail.com>
2015-09-02 10:52:32 -0400

    CoreLocation API check location authorisation status in ViewController

--- FreeWifiSearch/FreeWifiSearch/ViewController.swift
+++ FreeWifiSearch/FreeWifiSearch/ViewController.swift
@@ -7,18 +7,48 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-    
+
+    private var locationManager: CLLocationManager!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.locationManager = CLLocationManager()
+
+        // Delegate Object for updated events
+        self.locationManager.delegate = self
     }
-    
+
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+
+        self.checkLocationAuthorizationStatus()
+    }
+
+    private func checkLocationAuthorizationStatus() {
+        if CLLocationManager.authorizationStatus() == .AuthorizedWhenInUse {
+        } else {
+            self.locationManager.requestWhenInUseAuthorization()
+        }
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
+
+}
+
+/* CLLocationManagerDelegate Protocol defines methods to receive location and heading updates from CLLocationManager Object
+*/
+extension ViewController: CLLocationManagerDelegate {
+
+    // Communicates to Delegate that authorization status for app changed
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        self.checkLocationAuthorizationStatus()
+    }
+
 }
