92f1b5e4613ba0f4e9eef389fb683636a1cd91bd
Luke Schoen <ltfschoen@gmail.com>
2015-09-03 09:32:08 -0400

    Added Bridging Header file so Swift recognises Objective-C classes

---
+++ FreeWifiSearch/FreeWifiSearch/FreeWifiSearchObjCBridging.h
@@ -0,0 +1,16 @@
+//
+//  FreeWifiSearchObjCBridging.h
+//  FreeWifiSearch
+//
+//  Created by LS on 2/09/2015.
+//  Copyright © 2015 LS. All rights reserved.
+//
+
+#ifndef FreeWifiSearchObjCBridging_h
+#define FreeWifiSearchObjCBridging_h
+
+/* Import Facebook SDK so Swift recognises its referenced methods and Objective-C classes
+*/
+#import <FacebookSDK/FacebookSDK.h>
+
+#endif /* FreeWifiSearchObjCBridging_h */
